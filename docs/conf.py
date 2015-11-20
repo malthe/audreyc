d49e3bb3551c8c0e6f17f0860a43564914179234
Malthe Borch <mborch@gmail.com>
2015-11-20 23:02:40 +0100

    Fix project name

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'Audrey'
+project = 'Audrey Documentation'
 copyright = '2015, Malthe Borch'
 author = 'Malthe Borch'
 
